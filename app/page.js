9985e4e62bbd900369b48a9fbac62f17c70f5a79
brr <abdouplayz2003@gmail.com>
2025-01-29 22:27:07 +0100

    lesson why reach to watch

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
 <main>
 <h1>Collect Customer Feedback to build better products</h1>
 <div>Create a Feedback board in minutes, prioritize features, and build products your customers will love.</div>
+<Link className="btn" href="/dashboard">Dashboard</Link>
 </main>
   );
 }

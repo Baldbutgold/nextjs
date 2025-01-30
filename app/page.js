fccd63ba7662da1767f201605e08c2223297b130
brr <abdouplayz2003@gmail.com>
2025-01-30 11:53:30 +0100

    check

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
-import Link from "next/link";
+import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
   return (
-<main>
-<h1>Collect Customer Feedback to build better products</h1>
-<div>Create a Feedback board in minutes, prioritize features, and build products your customers will love.</div>
-<Link className="btn" href="/dashboard">Dashboard</Link>
-</main>
+    <main>
+      <h1>Collect Customer Feedback to build better products</h1>
+      <div>
+        Create a Feedback board in minutes, prioritize features, and build
+        products your customers will love.
+      </div>
+      <ButtonLogin />
+    </main>
   );
 }

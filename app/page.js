680195e8c38cd614dd166112d2098c735bd2aedc
Abderahman Elhadji <90966369+Baldbutgold@users.noreply.github.com>
2025-02-05 16:57:39 +0000

    t

--- app/page.js
+++ app/page.js
@@ -6,7 +6,9 @@ export default function Home() {
 
   return (
     <main>
-      <h1>Collect Customer Feedback to build better products</h1>
+      <h1 className="title extra-space">
+        Collect Customer Feedback to build better products
+      </h1>
       <div>
         Create a Feedback board in minutes, prioritize features, and build
         products your customers will love.

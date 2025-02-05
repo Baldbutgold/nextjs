529568b6f33874259b556aa4be5a6e2f2db20f0d
Abderahman Elhadji <90966369+Baldbutgold@users.noreply.github.com>
2025-02-05 20:42:19 +0000

    lesson 80 or so

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import ButtonLogin from "@/components/ButtonLogin";
 export default function Home() {
   const isLoggedIn = true;
   const name = "Dorthy";
-  const counter = 10;
 
   return (
     <main>
+      {/* HEADER */}
       <section className="bg-base-200">
         <div className="flex justify-between items-center px-8 py-4 max-w-5xl mx-auto">
           <div className="font-bold">CodeFast SaaS</div>
@@ -20,6 +20,7 @@ export default function Home() {
         </div>
       </section>
 
+      {/* HERO */}
       <section className="px-8 text-center py-32 max-w-5xl mx-auto">
         <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
           Collect Customer Feedback to build better products
@@ -29,6 +30,99 @@ export default function Home() {
           products your customers will love.
         </div>
         <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
+
+      {/* PRICING */}
+      <section className="bg-base-200">
+        <div className="px-8 py-32 max-w-5xl mx-auto">
+          <p className="uppercase font-medium text-center opacity-70 mb-4 text-primary">
+            pricing
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-bold mb-12 text-center">
+            A pricing that adapts to your needs
+          </h2>
+          <div className="p-8 bg-base-100 max-w-96 rounded-lg mx-auto space-y-6">
+            <div className="">
+              <div className="inline-block font-black text-4xl pr-0.5">
+                $300
+              </div>
+              <div className="inline-block text-sm font-medium opacity-60">
+                /month
+              </div>
+            </div>
+            <ul className="space-y-1.5">
+              <li className="flex gap-2 items-center mt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="text-success size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg>
+                Collect customer feedback
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="text-success size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg>
+                Unlimited boards
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="text-success size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg>
+                Admin Dashboard
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="text-success size-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg>
+                24/7
+              </li>
+            </ul>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          </div>
+        </div>
       </section>
     </main>
   );
